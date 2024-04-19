f4bb0842a9950046b6c4c416f9397ce9019af857
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-04-19 17:15:11 +0200

    add my logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
 ]
 ,
+theme: {
+  screens: {
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+  }
+}
 
 
 
